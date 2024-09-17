8a03deda0f91f6f534409a6de44f013647123127
Branden Hoskins <bghoskins16@gmail.com>
2024-09-17 10:11:24 -0600

    Created a piece move calculator class

---
+++ shared/src/main/java/chess/PieceMoveCalculator.java
@@ -0,0 +1,11 @@
+package chess;
+
+import java.util.Collection;
+
+public class PieceMoveCalculator {
+
+    public Collection<ChessMove> pieceMoveCalculator(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type, ChessBoard board, ChessPosition myPosition) {
+        throw new RuntimeException("Not implemented");
+    }
+
+}
