f6dda9b926f1506f45a83228d05e24c5e6838f77
Branden Hoskins <bghoskins16@gmail.com>
2024-09-18 15:17:43 -0600

    update to move calculator with mo piece defined

--- shared/src/main/java/chess/PieceMoveCalculator.java
+++ shared/src/main/java/chess/PieceMoveCalculator.java
@@ -5,7 +5,7 @@ import java.util.Collection;
 public class PieceMoveCalculator {
 
     public Collection<ChessMove> pieceMoveCalculator(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type, ChessBoard board, ChessPosition myPosition) {
-        throw new RuntimeException("Not implemented");
+        return null;
     }
 
 }
