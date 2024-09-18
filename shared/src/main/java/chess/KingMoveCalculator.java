06134b984b98ba27598e555d408c37e3d285d4e2
Branden Hoskins <bghoskins16@gmail.com>
2024-09-18 14:55:51 -0600

    Got king and paw working

--- shared/src/main/java/chess/KingMoveCalculator.java
+++ shared/src/main/java/chess/KingMoveCalculator.java
@@ -12,94 +12,116 @@ public class KingMoveCalculator extends PieceMoveCalculator {
         int startRow = myPosition.getRow();
         int startCol = myPosition.getColumn();
 
-        int endRow;
-        int endCol;
         ChessPosition endPos;
 
         // Try up and to the right
-        endRow = startRow;
-        endCol = startCol;
-        while (endRow > 1 && endCol < 8) {
-            // Change the move Location
-            endRow--;
-            endCol++;
-            endPos = new ChessPosition(endRow, endCol);
-
+        if (startRow > 1 && startCol > 1) {
+            endPos = new ChessPosition(startRow - 1, startCol - 1);
             // if this is not my own piece add to Collection
             if (board.getPiece(endPos) != null) {
                 ChessGame.TeamColor color = board.getPiece(endPos).getTeamColor();
                 if (color != pieceColor) {
                     moves.add(new ChessMove(myPosition, endPos, null));
                 }
-                break;
-            }
-            else {
+            } else {
                 moves.add(new ChessMove(myPosition, endPos, null));
             }
         }
 
-        // Try down and to the right
-        endRow = startRow;
-        endCol = startCol;
-        while (endRow < 8 && endCol < 8) {
-            // Change the move Location
-            endRow++;
-            endCol++;
-            endPos = new ChessPosition(endRow, endCol);
-
+        // Try up
+        if (startRow > 1) {
+            endPos = new ChessPosition(startRow - 1, startCol);
             // if this is not my own piece add to Collection
             if (board.getPiece(endPos) != null) {
                 ChessGame.TeamColor color = board.getPiece(endPos).getTeamColor();
                 if (color != pieceColor) {
                     moves.add(new ChessMove(myPosition, endPos, null));
                 }
-                break;
-            }
-            else {
+            } else {
                 moves.add(new ChessMove(myPosition, endPos, null));
             }
         }
 
         // Try up and to the left
-        endRow = startRow;
-        endCol = startCol;
-        while (endRow > 1 && endCol > 1) {
-            // Change the move Location
-            endRow--;
-            endCol--;
-            endPos = new ChessPosition(endRow, endCol);
-
+        if (startRow > 0 && startCol < 8) {
+            endPos = new ChessPosition(startRow - 1, startCol + 1);
             // if this is not my own piece add to Collection
             if (board.getPiece(endPos) != null) {
                 ChessGame.TeamColor color = board.getPiece(endPos).getTeamColor();
                 if (color != pieceColor) {
                     moves.add(new ChessMove(myPosition, endPos, null));
                 }
-                break;
+            } else {
+                moves.add(new ChessMove(myPosition, endPos, null));
             }
-            else {
+        }
+
+        // Try right
+        if (startCol > 1) {
+            endPos = new ChessPosition(startRow, startCol - 1);
+            // if this is not my own piece add to Collection
+            if (board.getPiece(endPos) != null) {
+                ChessGame.TeamColor color = board.getPiece(endPos).getTeamColor();
+                if (color != pieceColor) {
+                    moves.add(new ChessMove(myPosition, endPos, null));
+                }
+            } else {
+                moves.add(new ChessMove(myPosition, endPos, null));
+            }
+        }
+
+        // Try left
+        if (startCol < 8) {
+            endPos = new ChessPosition(startRow, startCol + 1);
+            // if this is not my own piece add to Collection
+            if (board.getPiece(endPos) != null) {
+                ChessGame.TeamColor color = board.getPiece(endPos).getTeamColor();
+                if (color != pieceColor) {
+                    moves.add(new ChessMove(myPosition, endPos, null));
+                }
+            } else {
+                moves.add(new ChessMove(myPosition, endPos, null));
+            }
+        }
+
+        // Try down and to the right
+        if (startRow < 8 && startCol > 1) {
+            endPos = new ChessPosition(startRow + 1, startCol - 1);
+            // if this is not my own piece add to Collection
+            if (board.getPiece(endPos) != null) {
+                ChessGame.TeamColor color = board.getPiece(endPos).getTeamColor();
+                if (color != pieceColor) {
+                    moves.add(new ChessMove(myPosition, endPos, null));
+                }
+            } else {
+                moves.add(new ChessMove(myPosition, endPos, null));
+            }
+        }
+
+        // Try down
+        if (startRow < 8) {
+            endPos = new ChessPosition(startRow + 1, startCol);
+            // if this is not my own piece add to Collection
+            if (board.getPiece(endPos) != null) {
+                ChessGame.TeamColor color = board.getPiece(endPos).getTeamColor();
+                if (color != pieceColor) {
+                    moves.add(new ChessMove(myPosition, endPos, null));
+                }
+            } else {
                 moves.add(new ChessMove(myPosition, endPos, null));
             }
         }
 
         // Try down and to the left
-        endRow = startRow;
-        endCol = startCol;
-        while (endRow < 8 && endCol > 1) {
-            // Change the move Location
-            endRow++;
-            endCol--;
-            endPos = new ChessPosition(endRow, endCol);
-
+        if (startRow < 8 && startCol < 8) {
+            endPos = new ChessPosition(startRow + 1, startCol + 1);
             // if this is not my own piece add to Collection
             if (board.getPiece(endPos) != null) {
                 ChessGame.TeamColor color = board.getPiece(endPos).getTeamColor();
                 if (color != pieceColor) {
                     moves.add(new ChessMove(myPosition, endPos, null));
                 }
-                break;
-            }
-            else {
+            } else {
                 moves.add(new ChessMove(myPosition, endPos, null));
             }
         }
--- shared/src/main/java/chess/PawnMoveCalculator.java
+++ shared/src/main/java/chess/PawnMoveCalculator.java
@@ -8,103 +8,91 @@ public class PawnMoveCalculator extends PieceMoveCalculator {
     @Override
     public Collection<ChessMove> pieceMoveCalculator(ChessGame.TeamColor pieceColor, ChessPiece.PieceType type, ChessBoard board, ChessPosition myPosition) {
         Collection<ChessMove> moves = new LinkedList<>();
-        
+
         int startRow = myPosition.getRow();
         int startCol = myPosition.getColumn();
 
         int endRow;
-        int endCol;
         ChessPosition endPos;
 
-        // Try up and to the right
-        endRow = startRow;
-        endCol = startCol;
-        while (endRow > 1 && endCol < 8) {
-            // Change the move Location
-            endRow--;
-            endCol++;
-            endPos = new ChessPosition(endRow, endCol);
-
-            // if this is not my own piece add to Collection
-            if (board.getPiece(endPos) != null) {
-                ChessGame.TeamColor color = board.getPiece(endPos).getTeamColor();
-                if (color != pieceColor) {
-                    moves.add(new ChessMove(myPosition, endPos, null));
-                }
-                break;
-            }
-            else {
-                moves.add(new ChessMove(myPosition, endPos, null));
-            }
+        if (pieceColor == ChessGame.TeamColor.WHITE) {
+            endRow = startRow + 1;
+        } else {
+            endRow = startRow - 1;
         }
 
-        // Try down and to the right
-        endRow = startRow;
-        endCol = startCol;
-        while (endRow < 8 && endCol < 8) {
-            // Change the move Location
-            endRow++;
-            endCol++;
-            endPos = new ChessPosition(endRow, endCol);
+        boolean promotion = (endRow == 1 || endRow == 8);
 
+        // Try up and to the right
+        if (startCol > 1) {
+            endPos = new ChessPosition(endRow, startCol - 1);
             // if this is not my own piece add to Collection
             if (board.getPiece(endPos) != null) {
                 ChessGame.TeamColor color = board.getPiece(endPos).getTeamColor();
                 if (color != pieceColor) {
-                    moves.add(new ChessMove(myPosition, endPos, null));
+                    if (promotion){
+                        moves.add(new ChessMove(myPosition, endPos, ChessPiece.PieceType.QUEEN));
+                        moves.add(new ChessMove(myPosition, endPos, ChessPiece.PieceType.ROOK));
+                        moves.add(new ChessMove(myPosition, endPos, ChessPiece.PieceType.BISHOP));
+                        moves.add(new ChessMove(myPosition, endPos, ChessPiece.PieceType.KNIGHT));
+                    }
+                    else{
+                        moves.add(new ChessMove(myPosition, endPos, null));
+                    }
                 }
-                break;
-            }
-            else {
-                moves.add(new ChessMove(myPosition, endPos, null));
             }
         }
 
         // Try up and to the left
-        endRow = startRow;
-        endCol = startCol;
-        while (endRow > 1 && endCol > 1) {
-            // Change the move Location
-            endRow--;
-            endCol--;
-            endPos = new ChessPosition(endRow, endCol);
-
+        if (startCol < 8) {
+            endPos = new ChessPosition(endRow, startCol + 1);
             // if this is not my own piece add to Collection
             if (board.getPiece(endPos) != null) {
                 ChessGame.TeamColor color = board.getPiece(endPos).getTeamColor();
                 if (color != pieceColor) {
-                    moves.add(new ChessMove(myPosition, endPos, null));
+                    if (promotion){
+                        moves.add(new ChessMove(myPosition, endPos, ChessPiece.PieceType.QUEEN));
+                        moves.add(new ChessMove(myPosition, endPos, ChessPiece.PieceType.ROOK));
+                        moves.add(new ChessMove(myPosition, endPos, ChessPiece.PieceType.BISHOP));
+                        moves.add(new ChessMove(myPosition, endPos, ChessPiece.PieceType.KNIGHT));
+                    }
+                    else{
+                        moves.add(new ChessMove(myPosition, endPos, null));
+                    }
                 }
-                break;
-            }
-            else {
-                moves.add(new ChessMove(myPosition, endPos, null));
             }
         }
 
-        // Try down and to the left
-        endRow = startRow;
-        endCol = startCol;
-        while (endRow < 8 && endCol > 1) {
-            // Change the move Location
-            endRow++;
-            endCol--;
-            endPos = new ChessPosition(endRow, endCol);
-
-            // if this is not my own piece add to Collection
-            if (board.getPiece(endPos) != null) {
-                ChessGame.TeamColor color = board.getPiece(endPos).getTeamColor();
-                if (color != pieceColor) {
+        // Try up
+        if (startRow > 1 && startCol > 1) {
+            endPos = new ChessPosition(endRow, startCol);
+            if (board.getPiece(endPos) == null) {
+                if (promotion){
+                    moves.add(new ChessMove(myPosition, endPos, ChessPiece.PieceType.QUEEN));
+                    moves.add(new ChessMove(myPosition, endPos, ChessPiece.PieceType.ROOK));
+                    moves.add(new ChessMove(myPosition, endPos, ChessPiece.PieceType.BISHOP));
+                    moves.add(new ChessMove(myPosition, endPos, ChessPiece.PieceType.KNIGHT));
+                }
+                else{
                     moves.add(new ChessMove(myPosition, endPos, null));
                 }
-                break;
-            }
-            else {
-                moves.add(new ChessMove(myPosition, endPos, null));
+
+                // check to see if it can move twice
+                if (pieceColor == ChessGame.TeamColor.WHITE && startRow == 2) {
+                    endPos = new ChessPosition(endRow + 1, startCol);
+                    if (board.getPiece(endPos) == null) {
+                        moves.add(new ChessMove(myPosition, endPos, null));
+                    }
+                }
+                else if (pieceColor == ChessGame.TeamColor.BLACK && startRow == 7) {
+                    endPos = new ChessPosition(endRow - 1, startCol);
+                    if (board.getPiece(endPos) == null) {
+                        moves.add(new ChessMove(myPosition, endPos, null));
+                    }
+                }
             }
         }
 
-        return moves;
+            return moves;
+        }
     }
-
-}
