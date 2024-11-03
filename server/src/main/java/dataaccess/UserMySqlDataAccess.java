8f08c112969776dbd46f51bcaa4e439c6f44db58
Branden Hoskins <bghoskins16@gmail.com>
2024-11-02 19:54:13 -0600

    Re format project

--- server/src/main/java/dataaccess/UserMySqlDataAccess.java
+++ server/src/main/java/dataaccess/UserMySqlDataAccess.java
@@ -5,13 +5,15 @@ import model.UserData;
 public class UserMySqlDataAccess implements UserDataAccess {
 
     //clear: A method for clearing all data from the database. This is used during testing.
-    public void clear(){}
+    public void clear() {
+    }
 
     //createUser: Create a new user.
-    public void createUser(UserData newUser){}
+    public void createUser(UserData newUser) {
+    }
 
     //getUser: Retrieve a user with the given username.
-    public UserData getUser(String username){
+    public UserData getUser(String username) {
         return null;
     }
 
