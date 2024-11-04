6f0cd1bbcdf5e25853eed923ba7bb642cb0bd47b
Branden Hoskins <bghoskins16@gmail.com>
2024-11-04 15:11:21 -0700

    Added a mySQL base class with a configure function

---
+++ server/src/main/java/dataaccess/MySqlDataAccess.java
@@ -0,0 +1,20 @@
+package dataaccess;
+
+public class MySqlDataAccess {
+
+
+    public final String[] createStatements = {""};
+
+    public void configureDatabase() throws DataAccessException {
+        DatabaseManager.createDatabase();
+        try (var conn = DatabaseManager.getConnection()) {
+            for (var statement : createStatements) {
+                try (var preparedStatement = conn.prepareStatement(statement)) {
+                    preparedStatement.executeUpdate();
+                }
+            }
+        } catch (Exception ex) {
+            throw new DataAccessException(String.format("Unable to configure database: %s", ex.getMessage()));
+        }
+    }
+}
