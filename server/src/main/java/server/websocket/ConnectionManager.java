7204276105787204861dbe5b7112db1394805209
Branden Hoskins <bghoskins16@gmail.com>
2024-12-03 21:36:56 -0700

    Stared getting connection manager and broadcast working

--- server/src/main/java/server/websocket/ConnectionManager.java
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -1,4 +1,41 @@
 package server.websocket;
 
+import org.eclipse.jetty.websocket.api.Session;
+import websocket.messages.ServerMessage;
+
+import java.util.ArrayList;
+import java.util.concurrent.ConcurrentHashMap;
+
+import java.io.IOException;
+
 public class ConnectionManager {
+
+    public final ConcurrentHashMap<String, Connection> connections = new ConcurrentHashMap<>();
+
+    public void add(String userName, Session session) {
+        var connection = new Connection(userName, session);
+        connections.put(userName, connection);
+    }
+
+    public void remove(String userName) {
+        connections.remove(userName);
+    }
+
+    public void broadcast(String excludeUserName, String message) throws IOException {
+        var removeList = new ArrayList<Connection>();
+        for (var c : connections.values()) {
+            if (c.session.isOpen()) {
+                if (!c.userName.equals(excludeUserName)) {
+                    c.send(message);
+                }
+            } else {
+                removeList.add(c);
+            }
+        }
+
+        // Clean up any connections that were left open.
+        for (var c : removeList) {
+            connections.remove(c.userName);
+        }
+    }
 }
--- server/src/main/java/server/websocket/WebSocketHandler.java
+++ server/src/main/java/server/websocket/WebSocketHandler.java
@@ -6,22 +6,32 @@ import org.eclipse.jetty.websocket.api.*;
 import websocket.commands.UserGameCommand;
 import websocket.messages.ServerMessage;
 
+import java.io.IOException;
+
 @WebSocket
 public class WebSocketHandler {
 
+    private final ConnectionManager connections = new ConnectionManager();
+
     @OnWebSocketMessage
-    public void onMessage(Session session, String message) throws Exception {
+    public void onMessage(Session session, String message) throws IOException  {
         System.out.printf("Received: %s\n", message);
 
 
         UserGameCommand action = new Gson().fromJson(message, UserGameCommand.class);
         switch (action.getCommandType()) {
-            case CONNECT -> System.out.println("Received a connect");
+            case CONNECT -> connect(action.getAuthToken(), session);
             case MAKE_MOVE -> System.out.println("Received a make move");
             case LEAVE -> System.out.println("Received a leave");
             case RESIGN -> message = "{ \"Notification\": \"resigned\" }";
         }
 
         session.getRemote().sendString(message);
+    }
+
+    private void connect(String auth, Session session) throws IOException {
+        System.out.println("Received a connect" +session.toString());
+        connections.add(auth, session);
+        connections.broadcast(session.toString(), "{ \"Notification\": \"New Session\" }");
     }
 }
